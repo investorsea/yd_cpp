c6262ccbc66aa6de74de06854a4cce2a24f4a857
YILINPC\ydai <yd489@nyu.edu>
2022-09-03 14:56:28 +0800

    're-og'

---
+++ source/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int result = 300;
+    //std::cin >> result;
+    std::cout << result << std::endl;
+    std::cout << "Hello World -1231412312312" << std::endl;
+    std::cout << "Hello Claire" << std::endl;
+    //TODO: study more
+    for (int i=0;i<10;i++){
+        std::cout<<i<<std::endl;
+    }   
+}
